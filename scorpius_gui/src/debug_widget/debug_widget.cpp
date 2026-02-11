3ad96b5edd24693eb6adc05040b3827a49c13b9e
Charles-robot <chucklafond2005@gmail.com>
2026-02-11 10:55:32 -0500

    Rename ui elements

--- scorpius_gui/src/debug_widget/debug_widget.cpp
+++ scorpius_gui/src/debug_widget/debug_widget.cpp
@@ -1,6 +1,7 @@
 #include "debug_widget.hpp"
 
-DebugWidget::DebugWidget(QWidget* parent_, size_t number_) : QWidget(parent_)
+DebugWidget::DebugWidget(QWidget* parent_, size_t number_):
+    QWidget(parent_)
 {
     _ui.setupUi(this);
     this->setPatteNumber(number_);
@@ -9,4 +10,13 @@ DebugWidget::DebugWidget(QWidget* parent_, size_t number_) : QWidget(parent_)
 void DebugWidget::setPatteNumber(size_t number_)
 {
     _ui.PatteNumber->setText(QStringLiteral("Patte #") + QString::number(number_));
+}
+
+void DebugWidget::setAngleHorizontal(float angle_)
+{    
+}
+
+void DebugWidget::setAngleVertical(float angle_)
+{
+
 }
