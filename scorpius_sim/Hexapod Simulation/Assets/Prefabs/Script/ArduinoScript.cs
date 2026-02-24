0d7c0e6d4e1a74ab3939f331a48bf7dfa36b2e04
Simon Pearson <114246751+SimonP-4@users.noreply.github.com>
2026-02-24 13:11:22 -0500

    Camera movements update

--- scorpius_sim/Hexapod Simulation/Assets/Prefabs/Script/ArduinoScript.cs
+++ scorpius_sim/Hexapod Simulation/Assets/Prefabs/Script/ArduinoScript.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.UIElements;
 
 public class ArduinoScript : MonoBehaviour
 {
@@ -28,6 +29,8 @@ public class ArduinoScript : MonoBehaviour
     public bool forward = true;
     public float forwardAngle = 45f;
     public float backwardAngle = -45f;
+    public float neutralAngle = 0f;
+    private int position = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -38,70 +41,172 @@ public class ArduinoScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Vertical servomotors
-        if (EnableVerticalServos)
+        // Position inputs
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            if (up)
-            {
-                float angle = Mathf.Clamp(-upAngle + 90f, -45f, 65f);
-                servoVertA.targetAngle = angle;
-                servoVertB.targetAngle = angle;
-                servoVertC.targetAngle = angle;
-                servoVertD.targetAngle = angle;
-                servoVertE.targetAngle = angle;
-                servoVertF.targetAngle = angle;
-            }
-            else
-            {
-                float angle = Mathf.Clamp(-downAngle + 90f, -45f, 65f);
-                servoVertA.targetAngle = angle;
-                servoVertB.targetAngle = angle;
-                servoVertC.targetAngle = angle;
-                servoVertD.targetAngle = angle;
-                servoVertE.targetAngle = angle;
-                servoVertF.targetAngle = angle;
-            }
+            position = 1;
         }
-        else
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            servoVertA.targetAngle = 0f;
-            servoVertB.targetAngle = 0f;
-            servoVertC.targetAngle = 0f;
-            servoVertD.targetAngle = 0f;
-            servoVertE.targetAngle = 0f;
-            servoVertF.targetAngle = 0f;
+            position = 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            position = 3;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            position = 4;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            position = 0;
         }
 
-        // Horizontal servomotors
-        if (EnableHorizontalServos)
+        // Position
+        float ua = Mathf.Clamp(-upAngle + 90f, -45f, 65f);
+        float da = Mathf.Clamp(-downAngle + 90f, -45f, 65f);
+        float na = Mathf.Clamp(-neutralAngle + 90f, -45f, 65f);
+        switch (position)
         {
-            if (forward)
-            {
+            case 1:
                 servoHorizA.targetAngle = forwardAngle;
-                servoHorizB.targetAngle = forwardAngle;
                 servoHorizC.targetAngle = forwardAngle;
-                servoHorizD.targetAngle = forwardAngle;
                 servoHorizE.targetAngle = forwardAngle;
-                servoHorizF.targetAngle = forwardAngle;
-            }
-            else
-            {
-                servoHorizA.targetAngle = backwardAngle;
                 servoHorizB.targetAngle = backwardAngle;
-                servoHorizC.targetAngle = backwardAngle;
                 servoHorizD.targetAngle = backwardAngle;
-                servoHorizE.targetAngle = backwardAngle;
                 servoHorizF.targetAngle = backwardAngle;
-            }
+                servoVertA.targetAngle = da;
+                servoVertB.targetAngle = da;
+                servoVertC.targetAngle = da;
+                servoVertD.targetAngle = da;
+                servoVertE.targetAngle = da;
+                servoVertF.targetAngle = da;
+                break;
+            case 2:
+                servoHorizA.targetAngle = neutralAngle;
+                servoHorizC.targetAngle = neutralAngle;
+                servoHorizE.targetAngle = neutralAngle;
+                servoHorizB.targetAngle = neutralAngle;
+                servoHorizD.targetAngle = neutralAngle;
+                servoHorizF.targetAngle = neutralAngle;
+                servoVertA.targetAngle = ua;
+                servoVertB.targetAngle = da;
+                servoVertC.targetAngle = ua;
+                servoVertD.targetAngle = da;
+                servoVertE.targetAngle = ua;
+                servoVertF.targetAngle = da;
+                break;
+            case 3:
+                servoHorizA.targetAngle = backwardAngle;
+                servoHorizC.targetAngle = backwardAngle;
+                servoHorizE.targetAngle = backwardAngle;
+                servoHorizB.targetAngle = forwardAngle;
+                servoHorizD.targetAngle = forwardAngle;
+                servoHorizF.targetAngle = forwardAngle;
+                servoVertA.targetAngle = da;
+                servoVertB.targetAngle = da;
+                servoVertC.targetAngle = da;
+                servoVertD.targetAngle = da;
+                servoVertE.targetAngle = da;
+                servoVertF.targetAngle = da;
+                break;
+            case 4:
+                servoHorizA.targetAngle = neutralAngle;
+                servoHorizC.targetAngle = neutralAngle;
+                servoHorizE.targetAngle = neutralAngle;
+                servoHorizB.targetAngle = neutralAngle;
+                servoHorizD.targetAngle = neutralAngle;
+                servoHorizF.targetAngle = neutralAngle;
+                servoVertA.targetAngle = da;
+                servoVertB.targetAngle = ua;
+                servoVertC.targetAngle = da;
+                servoVertD.targetAngle = ua;
+                servoVertE.targetAngle = da;
+                servoVertF.targetAngle = ua;
+                break;
+            case 0:
+                servoHorizA.targetAngle = neutralAngle;
+                servoHorizC.targetAngle = neutralAngle;
+                servoHorizE.targetAngle = neutralAngle;
+                servoHorizB.targetAngle = neutralAngle;
+                servoHorizD.targetAngle = neutralAngle;
+                servoHorizF.targetAngle = neutralAngle;
+                servoVertA.targetAngle = neutralAngle;
+                servoVertB.targetAngle = neutralAngle;
+                servoVertC.targetAngle = neutralAngle;
+                servoVertD.targetAngle = neutralAngle;
+                servoVertE.targetAngle = neutralAngle;
+                servoVertF.targetAngle = neutralAngle;
+                break;
+            default:
+                break;
         }
-        else
-        {
-            servoHorizA.targetAngle = 0f;
-            servoHorizB.targetAngle = 0f;
-            servoHorizC.targetAngle = 0f;
-            servoHorizD.targetAngle = 0f;
-            servoHorizE.targetAngle = 0f;
-            servoHorizF.targetAngle = 0f;
-        }
+
+        // Vertical servomotors
+        //if (EnableVerticalServos)
+        //{
+        //    if (up)
+        //    {
+        //        float angle = Mathf.Clamp(-upAngle + 90f, -45f, 65f);
+        //        servoVertA.targetAngle = angle;
+        //        servoVertB.targetAngle = angle;
+        //        servoVertC.targetAngle = angle;
+        //        servoVertD.targetAngle = angle;
+        //        servoVertE.targetAngle = angle;
+        //        servoVertF.targetAngle = angle;
+        //    }
+        //    else
+        //    {
+        //        float angle = Mathf.Clamp(-downAngle + 90f, -45f, 65f);
+        //        servoVertA.targetAngle = angle;
+        //        servoVertB.targetAngle = angle;
+        //        servoVertC.targetAngle = angle;
+        //        servoVertD.targetAngle = angle;
+        //        servoVertE.targetAngle = angle;
+        //        servoVertF.targetAngle = angle;
+        //    }
+        //}
+        //else
+        //{
+        //    servoVertA.targetAngle = 0f;
+        //    servoVertB.targetAngle = 0f;
+        //    servoVertC.targetAngle = 0f;
+        //    servoVertD.targetAngle = 0f;
+        //    servoVertE.targetAngle = 0f;
+        //    servoVertF.targetAngle = 0f;
+        //}
+
+        //// Horizontal servomotors
+        //if (EnableHorizontalServos)
+        //{
+        //    if (forward)
+        //    {
+        //        servoHorizA.targetAngle = forwardAngle;
+        //        servoHorizB.targetAngle = forwardAngle;
+        //        servoHorizC.targetAngle = forwardAngle;
+        //        servoHorizD.targetAngle = forwardAngle;
+        //        servoHorizE.targetAngle = forwardAngle;
+        //        servoHorizF.targetAngle = forwardAngle;
+        //    }
+        //    else
+        //    {
+        //        servoHorizA.targetAngle = backwardAngle;
+        //        servoHorizB.targetAngle = backwardAngle;
+        //        servoHorizC.targetAngle = backwardAngle;
+        //        servoHorizD.targetAngle = backwardAngle;
+        //        servoHorizE.targetAngle = backwardAngle;
+        //        servoHorizF.targetAngle = backwardAngle;
+        //    }
+        //}
+        //else
+        //{
+        //    servoHorizA.targetAngle = 0f;
+        //    servoHorizB.targetAngle = 0f;
+        //    servoHorizC.targetAngle = 0f;
+        //    servoHorizD.targetAngle = 0f;
+        //    servoHorizE.targetAngle = 0f;
+        //    servoHorizF.targetAngle = 0f;
+        //}
     }
 }
--- scorpius_sim/Hexapod Simulation/Assets/Prefabs/Script/ValueSubscriber.cs
+++ scorpius_sim/Hexapod Simulation/Assets/Prefabs/Script/ValueSubscriber.cs
@@ -1,26 +1,38 @@
-using UnityEngine;
-using Unity.Robotics.ROSTCPConnector;
 using RosMessageTypes.Std;
+using Unity.Robotics.ROSTCPConnector;
+using UnityEngine;
 
 
 public class ValueSubscriber : MonoBehaviour
 {
     ROSConnection ros;
-    private float receivedValue;
+    private float[] servoAngles = new float[12];
 
     void Start()
     {
         ros = ROSConnection.GetOrCreateInstance();
-        ros.Subscribe<Float32Msg>("my_value", Callback);
+        ros.Subscribe<Float32Msg>("ServoAngles", Callback);
     }
 
     void Callback(Float32Msg msg)
     {
-        receivedValue = msg.data;
+        //servoAngles[0] = msg.vert_a
+        //servoAngles[1] = msg.vert_b
+        //servoAngles[2] = msg.vert_c
+        //servoAngles[3] = msg.vert_d
+        //servoAngles[4] = msg.vert_e
+        //servoAngles[5] = msg.vert_f
+        //servoAngles[6] = msg.horiz_a
+        //servoAngles[7] = msg.horiz_b
+        //servoAngles[8] = msg.horiz_c
+        //servoAngles[9] = msg.horiz_d
+        //servoAngles[10] = msg.horiz_e
+        //servoAngles[11] = msg.horiz_f
+
     }
 
-    public float GetReceivedValue()
+    public float[] GetReceivedValue()
     {
-        return receivedValue;
+        return servoAngles;
     }
 }
