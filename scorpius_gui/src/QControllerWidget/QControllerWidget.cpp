b975f1755cc0c2963a5b3f1eba46c1aadbaf3058
Charles-robot <chucklafond2005@gmail.com>
2026-02-25 22:30:48 -0500

    draw X O CROSS TRIANGLE

--- scorpius_gui/src/QControllerWidget/QControllerWidget.cpp
+++ scorpius_gui/src/QControllerWidget/QControllerWidget.cpp
@@ -1,10 +1,11 @@
 #include "QControllerWidget.hpp"
 
 QControllerWidget::QControllerWidget(std::shared_ptr<rclcpp::Node> node_, QWidget* parent_):
-    QWidget(parent_), _node(node_)
+    QWidget(parent_),
+    _node(node_)
 {
     bool loadOk = _svgRenderer.load(QString(":/images/images/Dualshock_4_Layout.svg"));
-    if(!loadOk)
+    if (!loadOk)
     {
         RCLCPP_ERROR(_node->get_logger(), "SVG was not loaded");
     }
@@ -12,20 +13,33 @@ QControllerWidget::QControllerWidget(std::shared_ptr<rclcpp::Node> node_, QWidge
 
 void QControllerWidget::paintEvent(QPaintEvent*)
 {
-    if (_svgRenderer.isValid())
+    QPainter p(this);
+    p.setRenderHint(QPainter::Antialiasing);
+    p.setRenderHint(QPainter::SmoothPixmapTransform);
+
+    // draw the SVG to fit the widget while keeping aspect ratio
+    QRect targetRect = rect();
+    QSize svgSize = _svgRenderer.defaultSize();
+    svgSize.scale(targetRect.size(), Qt::KeepAspectRatio);
+
+    QPoint topLeft((width() - svgSize.width()) / 2, (height() - svgSize.height()) / 2);
+    QRect drawRect(topLeft, svgSize);
+
+    _svgRenderer.render(&p, drawRect);
+
+    p.setBrush(QBrush(QColor(173, 216, 230, 167)));
+    for (const Button& b : buttons)
     {
-        QPainter p(this);
-        p.setRenderHint(QPainter::Antialiasing);
-        p.setRenderHint(QPainter::SmoothPixmapTransform);
+        if (!b.pressed && false)
+        {
+            continue;
+        }
 
-        // draw the SVG to fit the widget while keeping aspect ratio
-        QRect targetRect = rect();
-        QSize svgSize = _svgRenderer.defaultSize();
-        svgSize.scale(targetRect.size(), Qt::KeepAspectRatio);
+        // convert normalized pos to actual coordinates
+        int x = drawRect.left() + b.posNorm.x() * drawRect.width();
+        int y = drawRect.top() + b.posNorm.y() * drawRect.height();
+        int radius = drawRect.width() * 0.03;  // 3% of SVG width
 
-        QPoint topLeft((width() - svgSize.width()) / 2, (height() - svgSize.height()) / 2);
-        QRect drawRect(topLeft, svgSize);
-
-        _svgRenderer.render(&p, drawRect);
+        p.drawEllipse(QPoint(x, y), radius, radius);
     }
 }
--- scorpius_gui/src/QControllerWidget/QControllerWidget.hpp
+++ scorpius_gui/src/QControllerWidget/QControllerWidget.hpp
@@ -5,6 +5,14 @@
 #include <QPainter>
 #include <QSvgRenderer>
 #include <rclcpp/rclcpp.hpp>
+#include <array>
+
+struct Button
+{
+    QString name;     // e.g. "cross", "circle"
+    QPointF posNorm;  // normalized x,y (0..1)
+    bool pressed;     // state from ROS2
+};
 
 class QControllerWidget : public QWidget
 {
@@ -17,6 +25,11 @@ class QControllerWidget : public QWidget
     void paintEvent(QPaintEvent*) override;
 
   private:
+    std::array<Button, 4> buttons = {{{"cross", QPointF(0.7875, 0.413), false},
+                                      {"circle", QPointF(0.8547, 0.3128), false},
+                                      {"square", QPointF(0.7205, 0.3128), false},
+                                      {"triangle", QPointF(0.7875, 0.2139), false}}};
+
     QSvgRenderer _svgRenderer;
     std::shared_ptr<rclcpp::Node> _node;
 };
