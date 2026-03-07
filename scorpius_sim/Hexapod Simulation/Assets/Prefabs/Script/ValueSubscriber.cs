ba183b46b52e26498c57e52d536eafd39aac3801
SimonP-4 <114246751+SimonP-4@users.noreply.github.com>
2026-03-07 14:41:47 -0500

    Comms still not working

--- scorpius_sim/Hexapod Simulation/Assets/Prefabs/Script/ValueSubscriber.cs
+++ scorpius_sim/Hexapod Simulation/Assets/Prefabs/Script/ValueSubscriber.cs
@@ -1,4 +1,4 @@
-using RosMessageTypes.Std;
+using RosMessageTypes.ScorpiusMain;
 using Unity.Robotics.ROSTCPConnector;
 using UnityEngine;
 
@@ -11,24 +11,24 @@ public class ValueSubscriber : MonoBehaviour
     void Start()
     {
         ros = ROSConnection.GetOrCreateInstance();
-        ros.Subscribe<Float32Msg>("ServoAngles", Callback);
+        ros.Subscribe<ServoAnglesMsg>("ServoAngles", Callback);
     }
 
-    void Callback(Float32Msg msg)
+    void Callback(ServoAnglesMsg msg)
     {
-        //servoAngles[0] = msg.vert_a
-        //servoAngles[1] = msg.vert_b
-        //servoAngles[2] = msg.vert_c
-        //servoAngles[3] = msg.vert_d
-        //servoAngles[4] = msg.vert_e
-        //servoAngles[5] = msg.vert_f
-        //servoAngles[6] = msg.horiz_a
-        //servoAngles[7] = msg.horiz_b
-        //servoAngles[8] = msg.horiz_c
-        //servoAngles[9] = msg.horiz_d
-        //servoAngles[10] = msg.horiz_e
-        //servoAngles[11] = msg.horiz_f
-
+        servoAngles[0] = msg.vert_a;
+        servoAngles[1] = msg.vert_b;
+        servoAngles[2] = msg.vert_c;
+        servoAngles[3] = msg.vert_d;
+        servoAngles[4] = msg.vert_e;
+        servoAngles[5] = msg.vert_f;
+        servoAngles[6] = msg.horiz_a;
+        servoAngles[7] = msg.horiz_b;
+        servoAngles[8] = msg.horiz_c;
+        servoAngles[9] = msg.horiz_d;
+        servoAngles[10] = msg.horiz_e;
+        servoAngles[11] = msg.horiz_f;
+        Debug.Log("Received servo angles: " + string.Join(", ", servoAngles));
     }
 
     public float[] GetReceivedValue()
