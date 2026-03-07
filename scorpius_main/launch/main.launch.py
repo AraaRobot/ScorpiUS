d47fcdea31cb6e3a5e2473862398945e412fefec
AraaRobot <anibal.arango@usherbrooke.ca>
2026-03-07 12:42:56 -0500

    Separating launch files

--- scorpius_main/launch/main.launch.py
+++ scorpius_main/launch/main.launch.py
@@ -10,16 +10,7 @@ def generate_launch_description():
     return LaunchDescription([
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([PathJoinSubstitution([FindPackageShare('scorpius_comm'), 'launch', 'comm.launch.py'])])),
-        
+
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([PathJoinSubstitution([FindPackageShare('scorpius_media'), 'launch', 'media.launch.py'])])),
-        
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([PathJoinSubstitution([FindPackageShare('scorpius_joy'), 'launch', 'joy.launch.py'])])),
-        
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([PathJoinSubstitution([FindPackageShare('scorpius_teleop'), 'launch', 'teleop.launch.py'])])),
-        
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([PathJoinSubstitution([FindPackageShare('scorpius_gui'), 'launch', 'gui.launch.py'])]))
+            PythonLaunchDescriptionSource([PathJoinSubstitution([FindPackageShare('scorpius_teleop'), 'launch', 'teleop.launch.py'])]))
     ])
