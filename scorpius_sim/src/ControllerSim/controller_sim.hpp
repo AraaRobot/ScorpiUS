5863299d95abf977eec780fd790641e5f87610c5
Anibal Arango <anibal.arango@usherbrooke.ca>
2026-03-21 23:38:34 -0400

    Basic simulation of ScorpiUS movement (#78)
    * Basic simulation of ScorpiUS movement
    
    * Removed magic value
    
    * copilot review

---
+++ scorpius_sim/src/ControllerSim/controller_sim.hpp
@@ -0,0 +1,40 @@
+#ifndef CONTROLLER_SIM_HPP
+#define CONTROLLER_SIM_HPP
+
+#include <rclcpp/rclcpp.hpp>
+#include <cstdint>
+
+#include "scorpius_main/msg/joy.hpp"
+
+enum class eSimState : uint8_t
+{
+    FORWARD = 0U,
+    BACKWARD,
+    LEFT,
+    RIGHT
+};
+
+class ControllerSim : public rclcpp::Node
+{
+    static constexpr const char* TOPIC_JOY_SIM = "/scorpius/joy";
+    static constexpr int64_t PUB_FREQ = 10;
+    static constexpr int64_t STATE_CHANGE_TIME = 5;
+
+  public:
+    ControllerSim();
+
+  private:
+    void simControllerPub_CB();
+    void updateState();
+    const char* getCurrentState();
+
+    rclcpp::Publisher<scorpius_main::msg::Joy>::SharedPtr _pub_controllerSim;
+    rclcpp::TimerBase::SharedPtr _timer_pub;
+    rclcpp::TimerBase::SharedPtr _timer_state;
+
+    scorpius_main::msg::Joy _lastSentMessage;
+
+    eSimState _currentState = eSimState::FORWARD;
+};
+
+#endif  // CONTROLLER_SIM_HPP
