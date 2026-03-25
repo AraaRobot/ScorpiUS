7b99e10e464dd51c02a309ba38f2dcab142b2236
Charles-Éloi Lafond <chucklafond2005@gmail.com>
2026-03-25 16:15:42 -0400

    Pi / Arduino serial communication V2 (#92)
    * remove c++ comm
    
    * Initial
    
    * Build and send pkg
    
    * Working but not tested on rpi
    
    * Create msg and srv files
    
    * Create subscription
    
    * Add support for serial read
    
    * Add msg type byte to micro + msg.md
    
    * Add serial write on arduino v/
    
    * Comm send v2
    
    * Add packet types to pi receiving end
    
    * fix build + lint
    
    * Copilot review
    
    * Copilot review 2
    
    * Review part 1
    
    * Add type safety
    
    * Typo

--- scorpius_micro/src/comm.cpp
+++ scorpius_micro/src/comm.cpp
@@ -8,7 +8,10 @@ TAIL 0xBB
 
 #include "comm.h"
 
-#define COMM_EXPECTED_LEN 12
+#define COMM_EXPECTED_LEN 13
+
+static const uint8_t HEAD = 0xAA;
+static const uint8_t TAIL = 0xBB;
 
 static HardwareSerial* commSerial = &Serial;
 static volatile int len = 0;
@@ -51,8 +54,10 @@ void comm_process()
                 len = b;
                 if (len == 0 || len > COMM_EXPECTED_LEN)
                 {
-                    commSerial->print("Invalid packet length: ");
-                    commSerial->print(len);
+                    COMM_DEBUG("Invalid packet length: ");
+                    COMM_DEBUG(len);
+                    static const uint8_t errPayload[1] = {static_cast<uint8_t>(eErrorCode::INVALID_LENGTH_RECEIVED)};
+                    comm_send(eSerialMsgType::ERROR, errPayload, 1);
                     state = WAIT_HEAD;
                 }
                 else
@@ -84,10 +89,13 @@ void comm_process()
                 else
                 {
                     // checksum mismatch -> discard
-                    commSerial->print("Checksum mismatch: got=");
-                    commSerial->print((int)b);
-                    commSerial->print(" expected=");
-                    commSerial->println((int)expected);
+                    COMM_DEBUG("Checksum mismatch: got=");
+                    COMM_DEBUG((int)b);
+                    COMM_DEBUG(" expected=");
+                    COMM_DEBUG((int)expected);
+
+                    static const uint8_t errPayload[1] = {static_cast<uint8_t>(eErrorCode::CHECKSUM_MISMATCH)};
+                    comm_send(eSerialMsgType::ERROR, errPayload, 1);
                     state = WAIT_HEAD;
                 }
                 break;
@@ -97,7 +105,10 @@ void comm_process()
                 {
                     if (packetReady)
                     {
-                        commSerial->print("Dropped a packet");
+                        COMM_DEBUG("Dropped a packet");
+
+                        static const uint8_t errPayload[1] = {static_cast<uint8_t>(eErrorCode::PACKET_DROPPED)};
+                        comm_send(eSerialMsgType::ERROR, errPayload, 1);
                     }
                     memcpy(packet, dataBuf, size_t(len));
                     packetLen = len;
@@ -128,14 +139,28 @@ bool comm_consume(sAngles& angles_)
 
     if (packetLen != COMM_EXPECTED_LEN)  // Can be change to a switch case if there a more message types in the future
     {
-        commSerial->print("comm_consume: bad len=");
-        commSerial->println(len);
+        COMM_DEBUG("comm_consume: bad len=");
+        COMM_DEBUG(packetLen);
+        static const uint8_t errPayload[1] = {static_cast<uint8_t>(eErrorCode::INVALID_LENGTH_RECEIVED)};
+        comm_send(eSerialMsgType::ERROR, errPayload, 1);
         packetReady = false;
         state = WAIT_HEAD;
         return false;
     }
 
     size_t index = 0;
+
+    if ((int8_t)packet[index++] != static_cast<uint8_t>(eSerialMsgType::COMMAND))
+    {
+        COMM_DEBUG("Unknown serial msg type");
+        static const uint8_t errPayload[1] = {static_cast<uint8_t>(eErrorCode::INVALID_MSG_TYPE_RECEIVED)};
+        comm_send(eSerialMsgType::ERROR, errPayload, 1);
+        packetReady = false;
+        state = WAIT_HEAD;
+        len = 0;
+        return false;
+    }
+
     angles_.vert_a = (int8_t)packet[index++];
     angles_.vert_b = (int8_t)packet[index++];
     angles_.vert_c = (int8_t)packet[index++];
@@ -155,7 +180,89 @@ bool comm_consume(sAngles& angles_)
     return true;
 }
 
+static bool comm_writePacket(uint8_t msgType_, const uint8_t* msgContent_, uint8_t msgContentLen_)
+{
+    if (!commSerial)
+    {
+        return false;
+    }
+
+    uint8_t writeLength = 1 + msgContentLen_;
+    uint8_t checksum = msgType_;
+
+    commSerial->write(HEAD);
+    commSerial->write(writeLength);
+    commSerial->write(msgType_);
+
+    for (uint8_t i = 0; i < msgContentLen_; ++i)
+    {
+        commSerial->write(msgContent_[i]);
+        checksum += msgContent_[i];
+    }
+
+    commSerial->write(checksum);
+    commSerial->write(TAIL);
+    return true;
+}
+
+bool comm_send(eSerialMsgType msgType_, const uint8_t* msgContent_, uint8_t contentLength_)
+{
+    if (!commSerial)
+    {
+        return false;
+    }
+
+    uint8_t expectedLen;
+    const uint8_t* contentPtr = nullptr;
+
+    switch (msgType_)
+    {
+        case eSerialMsgType::INFO:
+        [[fallthrough]]
+        case eSerialMsgType::ERROR:
+            expectedLen = 1;
+            if (msgContent_ == nullptr || contentLength_ != 1)
+            {
+                return false;
+            }
+            contentPtr = msgContent_;
+            break;
+
+        case eSerialMsgType::HEARTBEAT:
+            expectedLen = 0;
+            contentPtr = nullptr;
+            break;
+
+        default:
+            // convert unknown/CMD into ERROR(0x06)
+            msgType_ = eSerialMsgType::ERROR;
+            static const uint8_t invalidPayload[1] = {static_cast<uint8_t>(eErrorCode::TRIED_TO_SEND_INVALID_COMMAND)};
+            expectedLen = 1;
+            contentPtr = invalidPayload;
+            break;
+    }
+
+    return comm_writePacket(static_cast<uint8_t>(msgType_), contentPtr, expectedLen);
+}
+
 void comm_init(HardwareSerial& serial)
 {
     commSerial = &serial;
+}
+
+void comm_debug_impl(const char* msg)
+{
+    if (!ENABLE_DEBUG || commSerial == nullptr)
+    {
+        return;
+    }
+    commSerial->println(msg);
+}
+void comm_debug_impl(int v)
+{
+    if (!ENABLE_DEBUG || commSerial == nullptr)
+    {
+        return;
+    }
+    commSerial->println(v);
 }
