de8f20a81783a5a052e1c3e2050b1c05574bb344
etibel07 <etibel07@gmail.com>
2026-03-26 11:32:39 -0400

    Added the messages to the serial widget

--- scorpius_gui/src/QSerialWidget/QSerialManager.cpp
+++ scorpius_gui/src/QSerialWidget/QSerialManager.cpp
@@ -5,34 +5,49 @@ QSerialManager::QSerialManager(std::shared_ptr<rclcpp::Node> node_, QWidget* par
     _node(node_)
 {
     _grid = new QGridLayout(this);
-
     _button = new QPushButton("Connecter", this);
-
+    _label = new QLabel(this);
     _box = new QComboBox(this);
-    _box->addItem("N/A");
+    _scroll = new QScrollArea(this);
+
+    _box->addItem("Choisir un port série");
 
     _box->setItemData(0, Qt::AlignCenter, Qt::TextAlignmentRole);
+    _box->setMinimumHeight(50);
+
+    _grid->setColumnStretch(0, 3);
+    _grid->setColumnStretch(1, 1);
+    _grid->setRowStretch(0, 0);
+    _grid->setRowStretch(1, 1);
+
+    _grid->addWidget(_box, 0, 0);
+    _grid->addWidget(_button, 0, 1);
+    _grid->addWidget(_scroll, 1, 0, 1, 2);
+
+    _label->setText(" ");
+
+    _scroll->setWidget(_label);
+    _scroll->setWidgetResizable(true);
+    _scroll->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
     QFont boxFont = _box->font();
     boxFont.setPointSize(24);
     _box->setFont(boxFont);
 
-    _grid->addWidget(_box, 0, 0);
-
-    _label = new QLabel(this);
+    QFont buttonFont = _button->font();
+    buttonFont.setPointSize(24);
+    _button->setFont(buttonFont);
 
     QFont labelFont = _label->font();
-    labelFont.setPointSize(28);
+    labelFont.setPointSize(12);
     _label->setFont(labelFont);
-
-    _label->setText("Choisir un port série");
-    _label->setAlignment(Qt::AlignCenter);
-    _grid->addWidget(_label, 1, 0);
 
     setLayout(_grid);
 
     connect(this, &QSerialManager::serialPortsSignal, this, &QSerialManager::serialPortsSlot);
+    connect(this, &QSerialManager::serialStatusSignal, this, &QSerialManager::serialStatusSlot);
     connect(_box, QOverload<int>::of(&QComboBox::currentIndexChanged), this, &QSerialManager::comboBoxIndexChanged);
+    connect(_button, &QPushButton::clicked, this, &QSerialManager::pushButtonClicked);
 
     _sub_ports = _node->create_subscription<scorpius_main::msg::SerialPorts>("/scorpius/serial/ports",
                                                                              10,
@@ -40,6 +55,15 @@ QSerialManager::QSerialManager(std::shared_ptr<rclcpp::Node> node_, QWidget* par
                                                                              {
                                                                                  this->CB_subPorts(msg_);
                                                                              });
+    _sub_status
+        = _node->create_subscription<scorpius_main::msg::SerialStatus>("/scorpius/serial/status",
+                                                                       10,
+                                                                       [this](const scorpius_main::msg::SerialStatus& msg_)
+                                                                       {
+                                                                           this->CB_subStatus(msg_);
+                                                                       });
+
+    _srv_config = _node->create_client<scorpius_main::srv::SerialConfig>("/scorpius/serial/config");
 }
 
 QSerialManager::~QSerialManager() {}
@@ -49,11 +73,16 @@ void QSerialManager::CB_subPorts(const scorpius_main::msg::SerialPorts& msg_)
     emit this->serialPortsSignal(msg_.ports);
 }
 
+void QSerialManager::CB_subStatus(const scorpius_main::msg::SerialStatus& msg_)
+{
+    emit this->serialStatusSignal(msg_.message, msg_.ok);
+}
+
 void QSerialManager::serialPortsSlot(std::vector<std::string> port_name)
 {
     _box->clear();
 
-    _box->addItem("N/A");
+    _box->addItem("Choisir un port série");
     _box->setItemData(0, Qt::AlignCenter, Qt::TextAlignmentRole);
 
     if (!port_name.empty())
@@ -66,31 +95,60 @@ void QSerialManager::serialPortsSlot(std::vector<std::string> port_name)
     }
 }
 
+void QSerialManager::serialStatusSlot(std::string message, bool is_connected)
+{
+    _label->clear();
+    _label->setAlignment(Qt::AlignLeft);
+
+    const QString port_message = QString::fromStdString(message);
+
+    if (change_connection != is_connected)
+    {
+        if (is_connected && _box->currentIndex() != 0)
+        {
+            last_message += "Le port série est connecté\n";
+        }
+        else if (!is_connected && _box->currentIndex() != 0)
+        {
+            last_message += "Le port série est déconnecté\n";
+        }
+        else if (_box->currentIndex() == 0)
+        {
+            last_message = "";
+        }
+        change_connection = is_connected;
+    }
+
+    if (_box->currentIndex() != 0)
+    {
+        last_message += port_message + "\n";
+        _label->setText(last_message);
+    }
+
+    QFont labelFont = _label->font();
+    labelFont.setPointSize(24);
+    _label->setFont(labelFont);
+}
+
 void QSerialManager::comboBoxIndexChanged(int index)
 {
     if (index == 0)
     {
-        _button->hide();
-
-        QFont labelFont = _label->font();
-        labelFont.setPointSize(28);
-        _label->setFont(labelFont);
-
-        _label->setText("Choisir un port série");
+        _label->setText(" ");
         _label->setAlignment(Qt::AlignCenter);
-        _grid->addWidget(_label, 1, 0);
     }
     else
     {
-        _label->clear();
-
         _button->show();
 
-        _grid->addWidget(_button, 1, 0);
+        _label->setAlignment(Qt::AlignLeft);
+    }
+}
 
-        QFont buttonFont = _button->font();
-        buttonFont.setPointSize(40);
-        _button->setFont(buttonFont);
-        
+void QSerialManager::pushButtonClicked()
+{
+    if (_box->currentIndex() == 0)
+    {
+        return;
     }
 }
--- scorpius_gui/src/QSerialWidget/QSerialManager.hpp
+++ scorpius_gui/src/QSerialWidget/QSerialManager.hpp
@@ -8,6 +8,7 @@
 #include <QLabel>
 #include <QLineEdit>
 #include <QString>
+#include <QScrollArea>
 
 #include <rclcpp/rclcpp.hpp>
 #include "scorpius_main/srv/serial_config.hpp"
@@ -20,7 +21,7 @@ class QSerialManager : public QWidget
     Q_OBJECT
 
     static constexpr int ROWS = 2;
-    static constexpr int COLS = 1;
+    static constexpr int COLS = 2;
 
   public:
   QSerialManager(std::shared_ptr<rclcpp::Node> node_, QWidget* parent);
@@ -28,24 +29,34 @@ class QSerialManager : public QWidget
 
   private:
     void CB_subPorts(const scorpius_main::msg::SerialPorts& msg_);
+    void CB_subStatus(const scorpius_main::msg::SerialStatus& msg_);
+
+    QString last_message = "";
+    bool change_connection = false;
+
     std::shared_ptr<rclcpp::Node> _node;
 
     QGridLayout* _grid;
     QComboBox* _box;
     QPushButton* _button;
     QLabel* _label;
+    QScrollArea* _scroll;
 
     rclcpp::Subscription<scorpius_main::msg::SerialPorts>::SharedPtr _sub_ports;
+    rclcpp::Subscription<scorpius_main::msg::SerialStatus>::SharedPtr _sub_status;
     rclcpp::Publisher<scorpius_main::msg::SerialHeartbeat>::SharedPtr _pub_heartbeat;
     rclcpp::Publisher<scorpius_main::msg::SerialStatus>::SharedPtr _pub_status;
-    rclcpp::Service<scorpius_main::srv::SerialConfig>::SharedPtr _srv_config;
+    rclcpp::Client<scorpius_main::srv::SerialConfig>::SharedPtr _srv_config;
 
   signals:
         void serialPortsSignal(std::vector<std::string> port_name);
+        void serialStatusSignal(std::string message, bool is_connected);
     
   private slots:
         void serialPortsSlot(std::vector<std::string> port_name);
+        void serialStatusSlot(std::string message, bool is_connected);
         void comboBoxIndexChanged(int index);
+        void pushButtonClicked();
 };
 
 #endif
