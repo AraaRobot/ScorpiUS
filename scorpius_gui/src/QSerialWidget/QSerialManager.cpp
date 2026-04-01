53a92e04ba8f7b6236f429583213ef2dda3ea819
etibel07 <etibel07@gmail.com>
2026-04-01 15:41:55 -0400

    Connect and refresh button done (no test)

--- scorpius_gui/src/QSerialWidget/QSerialManager.cpp
+++ scorpius_gui/src/QSerialWidget/QSerialManager.cpp
@@ -6,23 +6,31 @@ QSerialManager::QSerialManager(std::shared_ptr<rclcpp::Node> node_, QWidget* par
 {
     _grid = new QGridLayout(this);
     _button = new QPushButton("Connecter", this);
+    _refresh = new QPushButton(this);
     _label = new QLabel(this);
     _box = new QComboBox(this);
     _scroll = new QScrollArea(this);
 
-    _box->addItem("Choisir un port série");
+    _box->addItem("Choose a serial port");
 
     _box->setItemData(0, Qt::AlignCenter, Qt::TextAlignmentRole);
-    _box->setMinimumHeight(50);
+    _box->setFixedHeight(50);
 
-    _grid->setColumnStretch(0, 3);
-    _grid->setColumnStretch(1, 1);
+    _refresh->setText("\u21BB");
+    _refresh->setFixedHeight(50);
+
+    _button->setFixedHeight(50);
+
+    _grid->setColumnStretch(0, 0.1);
+    _grid->setColumnStretch(1, 4);
+    _grid->setColumnStretch(2, 2);
     _grid->setRowStretch(0, 0);
     _grid->setRowStretch(1, 1);
 
-    _grid->addWidget(_box, 0, 0);
-    _grid->addWidget(_button, 0, 1);
-    _grid->addWidget(_scroll, 1, 0, 1, 2);
+    _grid->addWidget(_refresh, 0, 0);
+    _grid->addWidget(_box, 0, 1);
+    _grid->addWidget(_button, 0, 2);
+    _grid->addWidget(_scroll, 1, 0, 1, 3);
 
     _label->setText(" ");
 
@@ -38,6 +46,10 @@ QSerialManager::QSerialManager(std::shared_ptr<rclcpp::Node> node_, QWidget* par
     buttonFont.setPointSize(24);
     _button->setFont(buttonFont);
 
+    QFont refreshFont = _refresh->font();
+    refreshFont.setPointSize(24);
+    _refresh->setFont(refreshFont);
+
     QFont labelFont = _label->font();
     labelFont.setPointSize(12);
     _label->setFont(labelFont);
@@ -47,14 +59,11 @@ QSerialManager::QSerialManager(std::shared_ptr<rclcpp::Node> node_, QWidget* par
     connect(this, &QSerialManager::serialPortsSignal, this, &QSerialManager::serialPortsSlot);
     connect(this, &QSerialManager::serialStatusSignal, this, &QSerialManager::serialStatusSlot);
     connect(_box, QOverload<int>::of(&QComboBox::currentIndexChanged), this, &QSerialManager::comboBoxIndexChanged);
-    connect(_button, &QPushButton::clicked, this, &QSerialManager::pushButtonClicked);
+    connect(_button, &QPushButton::clicked, this, &QSerialManager::connectButtonClicked);
+    connect(_refresh, &QPushButton::clicked, this, &QSerialManager::refreshButtonClicked);
+    connect(this, &QSerialManager::buttonFinishedSignal, this, &QSerialManager::buttonFinishedSlot);
 
-    _sub_ports = _node->create_subscription<scorpius_main::msg::SerialPorts>("/scorpius/serial/ports",
-                                                                             10,
-                                                                             [this](const scorpius_main::msg::SerialPorts& msg_)
-                                                                             {
-                                                                                 this->CB_subPorts(msg_);
-                                                                             });
+    _srv_ports = _node->create_client<scorpius_main::srv::SerialPorts>("/scorpius/serial/ports");
     _sub_status
         = _node->create_subscription<scorpius_main::msg::SerialStatus>("/scorpius/serial/status",
                                                                        10,
@@ -68,17 +77,12 @@ QSerialManager::QSerialManager(std::shared_ptr<rclcpp::Node> node_, QWidget* par
 
 QSerialManager::~QSerialManager() {}
 
-void QSerialManager::CB_subPorts(const scorpius_main::msg::SerialPorts& msg_)
-{
-    emit this->serialPortsSignal(msg_.ports);
-}
-
 void QSerialManager::CB_subStatus(const scorpius_main::msg::SerialStatus& msg_)
 {
     emit this->serialStatusSignal(msg_.message, msg_.ok);
 }
 
-void QSerialManager::serialPortsSlot(std::vector<std::string> port_name)
+void QSerialManager::serialPortsSlot(const std::vector<std::string>& port_name)
 {
     _box->clear();
 
@@ -95,39 +99,23 @@ void QSerialManager::serialPortsSlot(std::vector<std::string> port_name)
     }
 }
 
-void QSerialManager::serialStatusSlot(std::string message, bool is_connected)
+void QSerialManager::serialStatusSlot(const std::string& message, bool is_connected)
 {
-    _label->clear();
-    _label->setAlignment(Qt::AlignLeft);
-
     const QString port_message = QString::fromStdString(message);
 
-    if (change_connection != is_connected)
+    if (is_connected)
     {
-        if (is_connected && _box->currentIndex() != 0)
-        {
-            last_message += "Le port série est connecté\n";
-        }
-        else if (!is_connected && _box->currentIndex() != 0)
-        {
-            last_message += "Le port série est déconnecté\n";
-        }
-        else if (_box->currentIndex() == 0)
-        {
-            last_message = "";
-        }
-        change_connection = is_connected;
+        emit this->writeMessage(port_message);
     }
+}
 
-    if (_box->currentIndex() != 0)
-    {
-        last_message += port_message + "\n";
-        _label->setText(last_message);
-    }
+void QSerialManager::writeMessage(const QString& message)
+{
+    last_message[current_status_index] = message;
+    current_status_index = (current_status_index + 1) % last_message.size();
 
-    QFont labelFont = _label->font();
-    labelFont.setPointSize(24);
-    _label->setFont(labelFont);
+    _label->setText(message + "\n");
+    _label->setAlignment(Qt::AlignLeft);
 }
 
 void QSerialManager::comboBoxIndexChanged(int index)
@@ -145,39 +133,97 @@ void QSerialManager::comboBoxIndexChanged(int index)
     }
 }
 
-void QSerialManager::pushButtonClicked()
+void QSerialManager::connectButtonClicked()
 {
     if (_box->currentIndex() == 0)
     {
         return;
     }
-    std::shared_ptr<scorpius_main::srv::SerialConfig::Request> request
-        = std::make_shared<scorpius_main::srv::SerialConfig::Request>();
 
-    request->port = _box->currentText().toStdString();
-    request->timeout = 1000;
-    request->baud = 115200;
+    rclcpp::Client<scorpius_main::srv::SerialConfig>::WeakPtr weakClient = _srv_config;
 
-    auto futureAndRequest = _srv_config->async_send_request(request);
-    std::future<std::shared_ptr<scorpius_main::srv::SerialConfig::Response>> future
-     = std::move(futureAndRequest.future);
+    std::weak_ptr<QSerialManager> weakThis = weak_from_this();
 
-    if (future.wait_for(std::chrono::milliseconds(1000)) == std::future_status::ready)
-    {
-        auto response = future.get();
-        if (response->result)
+    _executor.addTask(
+        [weakClient, weakThis]()
         {
-            last_message += "Port série connecté avec succès\n";
-        }
-        else
-        {
-            last_message += "Échec de la connexion au port série\n";
-        }
-    }
-    else
-    {
-        last_message += "Le serveur n'a pas répondu à temps\n";
-    }
+            rclcpp::Client<scorpius_main::srv::SerialConfig>::SharedPtr client = weakClient.lock();
+            std::shared_ptr widget = weakThis.lock();
 
-    _label->setText(last_message);
+            std::shared_ptr<scorpius_main::srv::SerialConfig::Request> request
+                = std::make_shared<scorpius_main::srv::SerialConfig::Request>();
+
+            request->port = widget->_box->currentText().toStdString();
+            request->timeout = 1;
+            request->baud = 115200;
+
+            if (!client || !widget)
+            {
+                return;
+            }
+
+            auto futureAndRequest = client->async_send_request(request);
+            std::future<std::shared_ptr<scorpius_main::srv::SerialConfig::Response>> future = std::move(futureAndRequest.future);
+
+            if (future.wait_for(std::chrono::milliseconds(1000)) == std::future_status::ready)
+            {
+                auto response = future.get();
+                if (response->result)
+                {
+                    emit widget->buttonFinishedSignal("Serial port connected with success\n");
+                }
+                else
+                {
+                    emit widget->buttonFinishedSignal("Serial port connection failed\n");
+                }
+            }
+            else
+            {
+                emit widget->buttonFinishedSignal("Connect button service failed to respond in time\n");
+            }
+        });
+}
+
+void QSerialManager::buttonFinishedSlot(const std::string& message)
+{
+    const QString port_message = QString::fromStdString(message);
+    emit this->writeMessage(port_message);
+}
+
+void QSerialManager::refreshButtonClicked()
+{
+    rclcpp::Client<scorpius_main::srv::SerialPorts>::WeakPtr weakClient = _srv_ports;
+    std::weak_ptr<QSerialManager> weakThis = weak_from_this();
+
+    _executor.addTask(
+        [weakClient, weakThis]()
+        {
+            rclcpp::Client<scorpius_main::srv::SerialPorts>::SharedPtr client = weakClient.lock();
+            std::shared_ptr widget = weakThis.lock();
+
+            std::shared_ptr<scorpius_main::srv::SerialPorts::Request> request
+                = std::make_shared<scorpius_main::srv::SerialPorts::Request>();
+
+            if (!client || !widget)
+            {
+                return;
+            }
+
+            auto futureAndRequest = client->async_send_request(request);
+
+            std::future<std::shared_ptr<scorpius_main::srv::SerialPorts::Response>> future = std::move(futureAndRequest.future);
+
+            if (future.wait_for(std::chrono::milliseconds(1000)) == std::future_status::ready)
+            {
+                auto response = future.get();
+
+                const std::vector<std::string> ports = response->ports;
+
+                emit widget->serialPortsSignal(ports);
+            }
+            else
+            {
+                emit widget->buttonFinishedSignal("Refresh button service failed to respond in time\n");
+            }
+        });
 }
--- scorpius_gui/src/QSerialWidget/QSerialManager.hpp
+++ scorpius_gui/src/QSerialWidget/QSerialManager.hpp
@@ -14,24 +14,28 @@
 #include "scorpius_main/srv/serial_config.hpp"
 #include "scorpius_main/msg/serial_heartbeat.hpp"
 #include "scorpius_main/msg/serial_status.hpp"
-#include "scorpius_main/msg/serial_ports.hpp"
+#include "scorpius_main/srv/serial_ports.hpp"
+#include "helpers/asyncExecutor.hpp"
 
-class QSerialManager : public QWidget
+class QSerialManager : public QWidget, public std::enable_shared_from_this<QSerialManager>
 {
     Q_OBJECT
 
     static constexpr int ROWS = 2;
-    static constexpr int COLS = 2;
+    static constexpr int COLS = 3;
 
   public:
   QSerialManager(std::shared_ptr<rclcpp::Node> node_, QWidget* parent);
   ~QSerialManager();
 
   private:
-    void CB_subPorts(const scorpius_main::msg::SerialPorts& msg_);
     void CB_subStatus(const scorpius_main::msg::SerialStatus& msg_);
 
-    QString last_message = "";
+    void writeMessage(const QString& message);
+
+    std::array<QString, 32> last_message;
+    int current_status_index = 0;
+    int current_port_index = 0;
     bool change_connection = false;
 
     std::shared_ptr<rclcpp::Node> _node;
@@ -39,24 +43,30 @@ class QSerialManager : public QWidget
     QGridLayout* _grid;
     QComboBox* _box;
     QPushButton* _button;
+    QPushButton* _refresh;
     QLabel* _label;
     QScrollArea* _scroll;
 
-    rclcpp::Subscription<scorpius_main::msg::SerialPorts>::SharedPtr _sub_ports;
+    AsyncExecutor _executor = AsyncExecutor();
+
+    rclcpp::Client<scorpius_main::srv::SerialPorts>::SharedPtr _srv_ports;
     rclcpp::Subscription<scorpius_main::msg::SerialStatus>::SharedPtr _sub_status;
     rclcpp::Publisher<scorpius_main::msg::SerialHeartbeat>::SharedPtr _pub_heartbeat;
     rclcpp::Publisher<scorpius_main::msg::SerialStatus>::SharedPtr _pub_status;
     rclcpp::Client<scorpius_main::srv::SerialConfig>::SharedPtr _srv_config;
 
   signals:
-        void serialPortsSignal(std::vector<std::string> port_name);
-        void serialStatusSignal(std::string message, bool is_connected);
+        void serialPortsSignal(const std::vector<std::string>& port_name);
+        void serialStatusSignal(const std::string& message, bool is_connected);
+        void buttonFinishedSignal(const std::string& message);
     
   private slots:
-        void serialPortsSlot(std::vector<std::string> port_name);
-        void serialStatusSlot(std::string message, bool is_connected);
+        void serialPortsSlot(const std::vector<std::string>& port_name);
+        void serialStatusSlot(const std::string& message, bool is_connected);
         void comboBoxIndexChanged(int index);
-        void pushButtonClicked();
+        void connectButtonClicked();
+        void refreshButtonClicked();
+        void buttonFinishedSlot(const std::string& message);
 };
 
 #endif
